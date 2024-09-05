94ffcb5a743c0d0435c172cc6a634af2258586a2
Ti <13039524+yang-mingyong@user.noreply.gitee.com>
2024-09-05 13:33:26 +0800

    .babelrc package-lock.json package.json src/main.js src/components/HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <u-button :ripple="true">十年</u-button>
+    <Button type="primary">Primary</Button>
   </div>
 </template>
 
@@ -9,9 +9,3 @@ export default {
   name: 'HelloWorld'
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss">
-	/* 注意要写在第一行，同时给style标签加入lang="scss"属性 */
-	@import "~uview-ui/theme.scss";
-</style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
-import { createApp } from 'vue'
-import ViewUIPlus from 'view-ui-plus'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'view-ui-plus/dist/styles/viewuiplus.css'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import Routers from './router';
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
 
-const app = createApp(App)
+Vue.use(VueRouter);
+Vue.use(ViewUI);
 
-app.use(store)
-  .use(router)
-  .use(ViewUIPlus)
-  .mount('#app')
+// The routing configuration
+const RouterConfig = {
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
+
+new Vue({
+    el: '#app',
+    router: router,
+    render: h => h(App)
+});
