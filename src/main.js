b29037c7b2479a088b440df704a329c3cf516e59
Ti <13039524+yang-mingyong@user.noreply.gitee.com>
2024-09-05 12:45:07 +0800

    package-lock.json package.json src/main.js

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'
-
-Vue.use(iView)
+import uView from "uview-ui"
+Vue.use(uView)
 Vue.config.productionTip = false
 
 new Vue({
